cfa07ec5270c1ff0f1933445d7230157e00bd52f
papangkorn00 <papangkorn.poomm@gmail.com>
2026-01-23 22:26:53 +0700

    feat: set-up db

--- config/db.go
+++ config/db.go
@@ -1,1 +1,46 @@
 package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// Global Variable for other packages
+var DB *gorm.DB
+
+func ConnectDB() *gorm.DB {
+	// 1. Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Warning: .env file not found. Using system env variables.")
+	}
+
+	// 2. Data Source Name
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_SSLMODE"),
+		os.Getenv("DB_TIMEZONE"),
+	)
+
+	// 3. Connect via GORM
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	// 4. Assign to global variable
+	DB = db
+	log.Println("PostgreSQL connected successfully!")
+
+	return db
+}
