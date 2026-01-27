7efa2253e23c5b943bf643e48c14d8430aa47c9a
papangkorn00 <papangkorn.poomm@gmail.com>
2026-01-27 22:31:05 +0700

    update json response to camelCase

--- backend/internal/models/crop.go
+++ backend/internal/models/crop.go
@@ -4,14 +4,14 @@ type Crop struct {
 	ID                 uint   `gorm:"primary_key" json:"id"`
 	Name               string `json:"name"`
 	Season             string `json:"season"`
-	GrowthTimeDay      int    `json:"growth_time_day"`
-	PossibleMaxHarvest int    `json:"possible_max_harvest"`
-	ImageURL           string `json:"image_url"`
-	PriceBase          int    `json:"buy_price_base"`
-	PriceBronze        int    `json:"buy_price_bronze"`
-	PriceSilver        int    `json:"buy_price_silver"`
-	PriceGold          int    `json:"buy_price_gold"`
-	PriceOsmium        int    `json:"buy_price_osmium"`
-	SellPrice          int    `json:"sell_price"`
-	GrowthTime         int    `json:"growth_time"`
+	GrowthTimeDay      int    `json:"growthTimeDay"`
+	PossibleMaxHarvest int    `json:"possibleMaxHarvest"`
+	ImageURL           string `json:"imageURL"`
+	PriceBase          int    `json:"buyPriceBase"`
+	PriceBronze        int    `json:"buyPriceBronze"`
+	PriceSilver        int    `json:"buyPriceSilver"`
+	PriceGold          int    `json:"buyPriceGold"`
+	PriceOsmium        int    `json:"buyPriceOsmium"`
+	SellPrice          int    `json:"sellPrice"`
+	GrowthTime         int    `json:"growthTime"`
 }
