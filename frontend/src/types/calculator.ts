cb277364cd5acecaafd73cba4fea646b7f294aa9
papangkorn00 <papangkorn.poomm@gmail.com>
2026-01-28 23:14:23 +0700

    feat: interface input and output

---
+++ frontend/src/types/calculator.ts
@@ -0,0 +1,27 @@
+import { type Crop } from "./crop";
+
+export interface CalculatorInput {
+    season: "Spring" | "Summer" | "Fall" | "Winter";
+    currentDay: number;
+    townRank: "F" | "E" | "D" | "C" | "B" | "A";
+    farmSize: number;
+}
+
+export interface CalculatorOutput {
+    crop: Crop
+
+    // Plant
+    plantCount: number;
+    seedCostTotal: number;
+
+    // Time
+    growthTime: number;
+    harvestCount: number;
+    remainingDays: number;
+
+    // Revenue
+    revenuePerHarvest: number;
+    totalRevenue: number;
+    netProfit: number;
+}
+
