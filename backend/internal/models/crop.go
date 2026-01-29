0a79ad18fed4d8caad2da105fa1a32f7818dbe3f
papangkorn00 <papangkorn.poomm@gmail.com>
2026-01-30 00:26:13 +0700

    fix:crop model field

--- backend/internal/models/crop.go
+++ backend/internal/models/crop.go
@@ -1,18 +1,20 @@
 package models
 
+import "github.com/lib/pq"
+
 type Crop struct {
-	ID                 uint   `gorm:"primary_key" json:"id"`
-	Name               string `json:"name"`
-	Season             string `json:"season"`
-	GrowthTimeDay      int    `json:"growthTimeDay"`
-	ReGrowthTimeDay    int    `json:"regrowthTimeDay"`
-	PossibleMaxHarvest int    `json:"possibleMaxHarvest"`
-	ImageURL           string `json:"imageURL"`
-	PriceBase          int    `json:"buyPriceBase"`
-	PriceBronze        int    `json:"buyPriceBronze"`
-	PriceSilver        int    `json:"buyPriceSilver"`
-	PriceGold          int    `json:"buyPriceGold"`
-	PriceOsmium        int    `json:"buyPriceOsmium"`
-	SellPrice          int    `json:"sellPrice"`
-	GrowthTime         int    `json:"growthTime"`
+	ID                    uint           `gorm:"primary_key" json:"id"`
+	Name                  string         `json:"name"`
+	Seasons               pq.StringArray `gorm:"type:text[]" json:"seasons"`
+	TownRank              string         `json:"townRank"`
+	GrowthTimeDay         int            `json:"growthTimeDay"`
+	RegrowthTimeDay       int            `json:"regrowthTimeDay"`
+	PossibleMaxHarvestDay int            `json:"possibleMaxHarvestDay"`
+	ImageURL              string         `json:"imageURL"`
+	SeedPrice             int            `json:"seedPrice"`
+	PriceBase             int            `json:"buyPriceBase"`
+	PriceBronze           int            `json:"buyPriceBronze"`
+	PriceSilver           int            `json:"buyPriceSilver"`
+	PriceGold             int            `json:"buyPriceGold"`
+	PriceOsmium           int            `json:"buyPriceOsmium"`
 }
