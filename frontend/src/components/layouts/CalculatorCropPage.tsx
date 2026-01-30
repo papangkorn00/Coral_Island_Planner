df8349d3d6dcca6c9eb43e18079f560a957846c9
papangkorn00 <papangkorn.poomm@gmail.com>
2026-01-30 15:44:41 +0700

    style: decor title

--- frontend/src/components/layouts/CalculatorCropPage.tsx
+++ frontend/src/components/layouts/CalculatorCropPage.tsx
@@ -18,11 +18,12 @@ const CalculatorCropPage = () => {
   // }, [])
 
   return (
-    <div className="relative min-h-screen bg-island-main bg-cover bg-center bg-fixed bg-no-repeat">
+    <div className="relative min-h-screen  bg-island-main bg-cover bg-center bg-fixed bg-no-repeat">
       <div className="absolute inset-0 bg-white/5 backdrop-blur-lg backdrop-brightness-75"></div>
-
-      <div className="relative flex justify-center z-10 p-10">
-        <h1 className="title font-coral-reef">Coral Island Planner</h1>
+      <div className="grid grid-cols-12">
+        <div className="relative col-span-12 mx-auto z-10 p-10">
+          <h1 className="title font-coral-reef">Coral Island Planner</h1>
+        </div>
       </div>
     </div>
   )
