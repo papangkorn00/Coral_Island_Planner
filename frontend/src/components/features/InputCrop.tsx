ef08289bb0bef54872c814c86f5b8bff190c7567
papangkorn00 <papangkorn.poomm@gmail.com>
2026-02-04 21:06:03 +0700

    style: input

--- frontend/src/components/features/InputCrop.tsx
+++ frontend/src/components/features/InputCrop.tsx
@@ -11,9 +11,9 @@ import {
   type SeasonValue,
   type TownRankValue,
 } from "@/types/crop"
-import {Input} from "../ui/input"
-
 import {ChevronDown} from "lucide-react"
+import {Input} from "@/components/ui/input"
+import {FieldLabel} from "@/components/ui/field"
 import {useState} from "react"
 
 const InputCrop = () => {
@@ -37,97 +37,103 @@ const InputCrop = () => {
   )
 
   return (
-    <div className="col-span-6 col-start-4 z-10 mt-12">
-      <div className="bg-(--bg-section) rounded-4xl h-24">
-        <div className="flex  justify-center-safe items-center-safe gap-x-28">
-          {/* Town Ranks */}
-          <div className="flex flex-col w-[200px]">
-            <label className="">Town Rank</label>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="outline" className="flex justify-between">
-                  {currentRank?.value}
-                  <ChevronDown className="ml-2 h-4 w-4 opacity-70" />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-[150px]">
-                {TOWN_RANKS.map((rank) => (
-                  <DropdownMenuItem
-                    key={rank.value}
-                    onSelect={() =>
-                      setInputState((prev) => ({
-                        ...prev,
-                        selectedRank: rank.value,
-                      }))
-                    }
-                  >
-                    {rank.label}
-                  </DropdownMenuItem>
-                ))}
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-          {/* Season */}
-          <div>
-            <label className="">Season</label>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button
-                  variant="outline"
-                  className="flex justify-between w-full"
+    <div className="col-span-6 col-start-4 z-10 mt-10">
+      <div className="flex justify-center-safe items-center-safe tracking-wide gap-x-28 bg-(--bg-section) rounded-4xl h-24">
+        {/* Town Ranks */}
+        <div>
+          <FieldLabel htmlFor="input-town-rank" className="font-coral-reef">
+            Town Rank
+          </FieldLabel>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant="outline"
+                className="flex justify-between w-[100px]"
+              >
+                {currentRank?.value}
+                <ChevronDown className="ml-2 h-4 w-4 opacity-70" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent id="input-town-rank" className="w-[100px]">
+              {TOWN_RANKS.map((rank) => (
+                <DropdownMenuItem
+                  key={rank.value}
+                  onSelect={() =>
+                    setInputState((prev) => ({
+                      ...prev,
+                      selectedRank: rank.value,
+                    }))
+                  }
                 >
-                  {currentSeason?.value}
-                  <ChevronDown className="ml-2 h-4 w-4 opacity-50" />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-[200px]">
-                {SEASON.map((season) => (
-                  <DropdownMenuItem
-                    key={season.value}
-                    onSelect={() =>
-                      setInputState((prev) => ({
-                        ...prev,
-                        selectedSeason: season.value,
-                      }))
-                    }
-                  >
-                    {season.label}
-                  </DropdownMenuItem>
-                ))}
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-          {/* Current Day */}
-          <div>
-            <label htmlFor="currentDay" className="">Current Day</label>
-            <Input
-              id="currentDay"
-              type="number"
-              aria-label="Current day (0-28)"
-              placeholder="Enter your current day"
-              min={0}
-              max={28}
-              onChange={(e) => {
-                const value = e.target.value
-                setInputState((prev) => ({...prev, currentDay: value}))
-              }}
-            ></Input>
-          </div>
-          {/* Farm size */}
-          <div>
-            <label htmlFor="farmSize" className="">Farm Size</label>
-            <Input
-              id="farmSize"
-              type="number"
-              aria-label="Farm Size"
-              placeholder="Enter your farm size"
-              min={0}
-              onChange={(e) => {
-                const value = e.target.value
-                setInputState((prev) => ({...prev, farmSize: value}))
-              }}
-            ></Input>
-          </div>
+                  {rank.label}
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+        {/* Season */}
+        <div>
+          <FieldLabel htmlFor="input-current-day" className="font-coral-reef">
+            Season
+          </FieldLabel>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" className="flex justify-between w-[100px]">
+                {currentSeason?.value}
+                <ChevronDown className="ml-2 h-4 w-4 opacity-50" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent id="input-current-day" className="w-[100px]">
+              {SEASON.map((season) => (
+                <DropdownMenuItem
+                  key={season.value}
+                  onSelect={() =>
+                    setInputState((prev) => ({
+                      ...prev,
+                      selectedSeason: season.value,
+                    }))
+                  }
+                >
+                  {season.label}
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+        {/* Current Day */}
+        <div>
+          <FieldLabel htmlFor="input-current-day" className="font-coral-reef">
+            Current Day
+          </FieldLabel>
+          <Input
+            className="w-[100px] bg-white"
+            id="input-current-day"
+            type="number"
+            defaultValue={1}
+            min={1}
+            max={28}
+            onChange={(e) => {
+              const value = e.target.value
+              setInputState((prev) => ({...prev, currentDay: value}))
+            }}
+          ></Input>
+        </div>
+        {/* Farm size */}
+        <div>
+          <FieldLabel htmlFor="input-farm-size" className="font-coral-reef">
+            Farm Size
+          </FieldLabel>
+          <Input
+            className="w-[100px] bg-white"
+            id="input-farm-size"
+            type="number"
+            defaultValue={1}
+            min={1}
+            onChange={(e) => {
+              const value = e.target.value
+              setInputState((prev) => ({...prev, farmSize: value}))
+            }}
+          ></Input>
         </div>
       </div>
     </div>
