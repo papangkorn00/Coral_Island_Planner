d12c9f12ba3f89d4143d0f2ff153bcbda9a460b4
papangkorn00 <papangkorn.poomm@gmail.com>
2026-02-05 23:53:10 +0700

    feat: store state for inputs and result

--- frontend/src/pages/CalculatorCropPage.tsx
+++ frontend/src/pages/CalculatorCropPage.tsx
@@ -1,15 +1,34 @@
 import Title from "@/components/layouts/Title"
 import ChartResult from "@/components/features/ChartResult"
 import InputCrop from "@/components/features/InputCrop"
+import type {CalculatorInput} from "@/types/calculator"
+import {useMemo, useState} from "react"
+import type {Crop} from "@/types/crop"
+import {calculateCrop} from "@/utils/calculateCrop"
 
 const CalculatorCropPage = () => {
+  const [inputs, setInputs] = useState<CalculatorInput>({
+    townRank: "F",
+    season: "Spring",
+    currentDay: 1,
+    farmSize: 1,
+  })
+
+  const [crops, setCrops] = useState<Crop[]>([])
+
+  const chartDataCalculate = useMemo(() => {
+    return crops.map((crop) => {
+      calculateCrop(crop, inputs)
+    })
+  }, [crops, inputs])
+
   return (
     <div className="relative min-h-screen bg-island-main bg-cover bg-center bg-fixed bg-no-repeat">
       <div className="absolute inset-0 bg-white/5 backdrop-blur-lg backdrop-brightness-75"></div>
       <div className="grid grid-cols-12">
         <Title />
 
-        <ChartResult />
+        <ChartResult  />
         <InputCrop />
       </div>
     </div>
